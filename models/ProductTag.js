ba2f252669823edaf119ee33929cf45e26faba90
naomi <naomilounsbury@hotmail.com>
2022-02-01 14:24:36 -0800

    made my four models

---
+++ models/ProductTag.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define an id column
+    id: {
+      // use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true
+    },
+    // define a username column
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'product',
+          key: 'id'
+        }
+      }
+    },
+    // define an email column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'tag',
+          key: 'id'
+        }
+      }
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
