72810f385c08c00f04d138e2ca196cf22f7c9db3
naomi <naomilounsbury@hotmail.com>
2022-02-01 17:30:27 -0800

    i haven't got something in the connection, put my methods in place

--- models/index.js
+++ models/index.js
@@ -5,13 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+// create associations
+Product.belongsTo(Category, {
+  foreignKey: 'catagory_id'
+});
 
 // Categories have many Products
-
+// product_id is in tags so I don't think this is rigth
+Catagory.hasMany(Product, {
+  foreignKey: 'catagory_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  //as: 'tagged_product',
+  foreignKey: 'tag_id'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  //as: 'product_tagged',
+  foreignKey: 'product_id'
+});
 module.exports = {
   Product,
   Category,
